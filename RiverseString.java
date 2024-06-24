44a18ac6fe080914acab4c027f2ba22bf498263f
Shaikshavali <shaikshasiddu97@gmail.com>
2024-06-24 15:27:13 +0530

    third commit

---
+++ RiverseString.java
@@ -0,0 +1,25 @@
+public class RiverseString {
+public static void main(String[]args){
+
+//reverse string
+
+
+String str="apple";
+StringBuffer sb=new StringBuffer(str);
+System.out.println(sb.reverse());
+
+
+
+//Swapping numbers
+int a=10;
+int b=20;
+a=a+b;
+b=a-b;
+a=a-b;
+System.out.println(a);
+System.out.println(b);
+
+
+
+}
+}
